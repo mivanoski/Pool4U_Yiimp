df497f18b42bf0540eeb1f89203e1cc319d824f8
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-04-20 21:50:09 +0200

    Changed!!! // todo: enhance/detect payout_max from normal sendmany error

--- web/yaamp/core/backend/payment.php
+++ web/yaamp/core/backend/payment.php
@@ -58,7 +58,9 @@ function BackendCoinPayments($coin)
     $users = getdbolist('db_accounts', "balance>$min_payout AND coinid={$coin->id} ORDER BY balance DESC");
 
     // todo: enhance/detect payout_max from normal sendmany error
-    if($coin->symbol == 'BITC' || $coin->symbol == 'BNODE' || $coin->symbol == 'BOD' || $coin->symbol == 'DIME' || $coin->symbol == 'BTCRY' || $coin->symbol == 'IOTS' || $coin->symbol == 'ECC' || $coin->symbol == 'BCRS' || $coin->symbol == 'SAPP' || $coin->symbol == 'CURVE' || !empty($coin->payout_max))
+    //if($coin->symbol == 'BITC' || $coin->symbol == 'BNODE' || $coin->symbol == 'BOD' || $coin->symbol == 'DIME' || $coin->symbol == 'BTCRY' || $coin->symbol == 'IOTS' || $coin->symbol == 'ECC' || $coin->symbol == 'BCRS' || $coin->symbol == 'SAPP' || $coin->symbol == 'CURVE' || !empty($coin->payout_max))
+    //Changed from Pool4U
+    if (!empty($coin->payout_max))
     {
         foreach ($users as $user) {
             $user = getdbo('db_accounts', $user->id);
